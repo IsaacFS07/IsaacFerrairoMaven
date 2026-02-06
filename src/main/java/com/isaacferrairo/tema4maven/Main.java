af1df269d73d7d913c55f5638bcf5371a2d705c6
isaac <isaacferrairo@gmail.com>
2026-02-06 19:12:33 +0100

    Generar el banner ASCII con JFiglet

--- src/main/java/com/isaacferrairo/tema4maven/Main.java
+++ src/main/java/com/isaacferrairo/tema4maven/Main.java
@@ -1,8 +1,12 @@
 package com.isaacferrairo.tema4maven;
-
+import com.github.lalyos.jfiglet.FigletFont;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) throws Exception {
+
+        String texto = "Isaac Ferrairo";
+        String banner = FigletFont.convertOneLine(texto);
+        System.out.println(banner);
 
         }
     }
