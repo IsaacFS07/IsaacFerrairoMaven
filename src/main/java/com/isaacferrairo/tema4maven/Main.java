28cfcf3ae2da992be2a0f6d3e0a176d6470d3691
isaac <isaacferrairo@gmail.com>
2026-02-06 19:53:21 +0100

    Construir el contenido completo del guion

--- src/main/java/com/isaacferrairo/tema4maven/Main.java
+++ src/main/java/com/isaacferrairo/tema4maven/Main.java
@@ -1,12 +1,33 @@
 package com.isaacferrairo.tema4maven;
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        //DEFINIMOS VARIABLES
         String texto = "Isaac Ferrairo";
         String banner = FigletFont.convertOneLine(texto);
-        System.out.println(banner);
+        List<String> infoTotal=new ArrayList<>();
+
+
+
+        for(String info : banner.split("\n")){
+            infoTotal.add(info);
+        }
+
+        infoTotal.add("NOMBRE: Isaac Ferrairó Sanfélix");
+        infoTotal.add("CORREO ELECTRONICO: isaacestudiante@mariaenriquez.com");
+        infoTotal.add("TELEFONO: 658775848");
+        infoTotal.add("VEHICULO: MOTOCICLETA PROPIA");
+        infoTotal.add("DIRECCIÓN: Rausell 2, Gandia, Valencia");
+        infoTotal.add("EXPERIENCIA LABORAL: 3 meses de practicas en la empresa SKLUM");
+        infoTotal.add("IDIOMAS: Español C2, Ingles B2");
+        infoTotal.add("LENGUAJES: JAVASCRIPT, HTML, CSS, JAVA");
+        infoTotal.add("EDAD: 18 AÑOS");
+        infoTotal.add("PERFIL PROFESIONAL: Estudiante de DAW");
+        infoTotal.forEach(System.out::println);
 
         }
     }
